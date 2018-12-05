e0d109572c828e9f448215b995808be9481c3786
igo21 <35313561+igo21@users.noreply.github.com>
2018-12-05 16:47:32 -0400

    setup

--- setup.py
+++ setup.py
@@ -1,27 +1,34 @@
-from cx_Freeze import setup, Executable
+
 import sys
+from cx_Freeze import setup, Executable
+import os
+
+os.environ['TCL_LIBRARY'] ="C:\\Users\\Igor\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
+os.environ['TK_LIBRARY'] = "C:\\Users\\Igor\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
 
 base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
-if sys.platform == 'win32':
-    base = None
+executable = [
+        Executable(script="Iniciar.py", base=base, icon="imagem/logoicone3.ico")
+]
+
+buildOptions = dict(
+        packages = ["tkinter"],
+        includes = ["tkinter","matplotlib","os","random","sys","functools","clipboard"],
+        include_files = ["logoicone4.ico","gerador.py"],
+        include_msvcr = True,
+        excludes = []
+)
 
 
-executables = [Executable("gerador.py", base=base)]
 
-packages = ["idna"]
-options = {
-    'build_exe': {
-
-        'packages':packages,
-    },
-
-}
 
 setup(
-    name = "Gerador de Get/Set",
-    options = options,
+    name = "GNUScasinos",
     version = "1.0",
-    description = 'Este software foi feito com muito carinho para aqueles que precisam gerar get/set',
-    executables = executables
-)
+    description = "Simulador de casinos",
+    options = dict(build_exe = buildOptions),
+    executables = executable
+ )
